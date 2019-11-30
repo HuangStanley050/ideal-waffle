75ccdf41d67a0b4c9b8d78a0ef45ea9a57c39318
Stanley Huang <huang.stanley050@gmail.com>
2019-11-30 20:00:31 +1100

    writing a unqiue key for redis cache

--- services/cache.js
+++ services/cache.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const exec = mongoose.Query.prototype.exec;
 
 mongoose.Query.prototype.exec = function() {
-  console.log("I am going to run a query");
+  const key = { ...this.getQuery(), collection: this.mongoose.Collection.name };
+  console.log(key);
   return exec.apply(this, arguments);
 };
