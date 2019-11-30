f8f7c3bf36b36d7d01f7b6b1bd8c6d90fd48f602
Stanley Huang <huang.stanley050@gmail.com>
2019-11-30 20:26:02 +1100

    still some error with the redis key

--- services/cache.js
+++ services/cache.js
@@ -1,9 +1,23 @@
 const mongoose = require("mongoose");
-
+const redis = require("redis");
+const util = require("util");
+const redisUrl = "redis://localhost:6379";
+const client = redis.createClient(redisUrl);
+client.get = util.promisify(client.get);
 const exec = mongoose.Query.prototype.exec;
 
-mongoose.Query.prototype.exec = function() {
-  const key = { ...this.getQuery(), collection: this.mongoose.Collection.name };
-  console.log(key);
-  return exec.apply(this, arguments);
+mongoose.Query.prototype.exec = async function() {
+  const key = JSON.stringify({
+    ...this.getQuery().id,
+    collection: this.mongooseCollection.name
+  });
+
+  const cacheValue = await client.get(key);
+  if (cacheValue) {
+    console.log(cacheValue);
+    return JSON.parse(cacheValue);
+  }
+  const result = await exec.apply(this, arguments);
+  client.set(key, JSON.stringify(result));
+  return result;
 };
