89c9e7858652e2aaac1402d65eea3147ffafbe43
Stanley Huang <huang.stanley050@gmail.com>
2019-11-30 20:58:45 +1100

    added chain cache function, again reviewing the method chaining

--- services/cache.js
+++ services/cache.js
@@ -6,7 +6,14 @@ const client = redis.createClient(redisUrl);
 client.get = util.promisify(client.get);
 const exec = mongoose.Query.prototype.exec;
 
+mongoose.Query.prototype.cache = function() {
+  this.useCache = true;
+  return this;
+};
 mongoose.Query.prototype.exec = async function() {
+  if (this.useCache === false) {
+    return exec.apply(this, arguments);
+  }
   const key = JSON.stringify({
     ...this.getQuery().id,
     collection: this.mongooseCollection.name
